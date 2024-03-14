434c7c60d3203d82e6e0a7a31716c4eb02d2ff16
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-03-14 14:05:24 +0100

    MIN-41
    MIN-41 login and sign up created for frontend and semi styled

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,27 +1,31 @@
 import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/movies">Movies</NavLink>
-          </li>
-          <li>
-            <NavLink to="/tickets">Tickets</NavLink>
-          </li>
-          <li>
-            <NavLink to="/upcoming">Coming Soon</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li>
-            <NavLink to="/signup">Sign Up</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <nav>
+                <ul>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <li>
+                                <NavLink to="/movies">Movies</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/tickets">Tickets</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/upcoming">Coming Soon</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/login">Login</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/signup">Sign Up</NavLink>
+                            </li>
+                        </div>
+                    </div>
+                </ul>
+            </nav>
+        </>
+    );
 }
