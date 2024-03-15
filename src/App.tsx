358a7012b5ece39c9f6bf89d6655a1f9286946bf
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-03-15 14:06:57 +0100

    MIN-35
    MIN-35 movie detail view virker

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import NavBar from './NavBar';
+import NavBar from "./NavBar";
 import Movies from "./components/Movies";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-
+import MovieDetailView from "./components/MovieDetailView";
 
 export default function App() {
-  return (
-    <>
-    <NavBar/>
-      <Routes>
-        <Route path="/movies" element={<Movies/>} />
-        <Route path="/tickets" element={<p>Tickets</p>} />
-        <Route path="/upcoming" element={<p>Coming Soon</p>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
-      </Routes>
-      </>
-  );
+    return (
+        <>
+            <NavBar />
+            <Routes>
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/tickets" element={<p>Tickets</p>} />
+                <Route path="/upcoming" element={<p>Coming Soon</p>} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/movies/:id" element={<MovieDetailView />} />
+            </Routes>
+        </>
+    );
 }
-
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,26 +1,31 @@
 import { useEffect, useState } from "react";
 import { getMovies } from "../services/apiFacade";
 import { Movie } from "../services/entityFacade";
+import { Link } from "react-router-dom";
 
 export default function Movies() {
-  const [movies, setMovies] = useState<Array<Movie> | null>(null);
+    const [movies, setMovies] = useState<Array<Movie> | null>(null);
 
-  useEffect(() => {
-    getMovies().then((data) => setMovies(data));
-  }, []);
+    useEffect(() => {
+        getMovies().then((data) => setMovies(data));
+    }, []);
 
-  return (
-    <>
-      <ul>
-        <h2>Movies</h2>
-        {movies ? (
-          movies.map((movie) => {
-            return <li key={movie.id}>{movie.title}</li>;
-          })
-        ) : (
-          <p>Loading...</p>
-        )}
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <ul>
+                <h2>Movies</h2>
+                {movies ? (
+                    movies.map((movie) => {
+                        return (
+                            <li key={movie.id}>
+                                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                            </li>
+                        );
+                    })
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </ul>
+        </>
+    );
 }
