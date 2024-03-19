98357daf3abac277819f2f2d89d44284e0a8f7bc
Slumhunden <kroghbastin@gmail.com>
2024-03-19 13:52:09 +0100

    MIN-33
    Users oversigt frontend styled somewhat

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -9,14 +9,14 @@ export default function MoviePoster({ movie }: { movie: Movie }) {
         <Link to={`/movies/${movie.id}`}>
           <img className="h-64 object-cover" src={movie.posterUrl} alt={movie.title} />
         </Link>
-        <div className="">
+        <div className="flex space-x-4">
           <Link to={`/movies/${movie.id}`} className="">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
               Details
             </button>
             <Link to={`/tickets/${movie.id}`}>
               <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                Buy Ticket
+                Tickets
               </button>
             </Link>
           </Link>
