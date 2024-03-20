063c3fdbe26b10a08bcd1059c6396a0edbebbeb9
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-03-20 11:09:36 +0100

    MIN-39
    MIN-39

--- src/components/UpComingMovies.tsx
+++ src/components/UpComingMovies.tsx
@@ -1,20 +1,12 @@
-import { getMovies } from "../services/apiFacade";
-import { useEffect, useState } from "react";
 import { Movie } from "../services/entityFacade";
 
-export default function UpComingMovies() {
-    const [movies, setMovies] = useState<Movie[]>([]);
-    useEffect(() => {
-        getMovies().then((data) => {
-            const upcomingMovies = data.filter((movie: Movie) => new Date(movie.releaseDate) > new Date());
-            setMovies(upcomingMovies);
-            console.log("Upcoming " + upcomingMovies);
-            console.log("movies " + movies);
-        });
-    }, []);
+interface Props {
+    movies: Movie[];
+}
 
+export default function UpComingMovies({ movies }: Props) {
     return (
-        <div className="w-64 h-85 pl-10 pb-10 grid grid-rows-2">
+        <div>
             {movies.map((movie) => (
                 <img src={movie.posterUrl} key={movie.id} />
             ))}
--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -12,7 +12,7 @@ export default function MoviesPage() {
     return (
         <div className="bg-kino-blue min-h-screen">
             <h1 className="text-center">Movies</h1>
-            <div className="grid grid-cols-4 gap-2 p-10 justify-items-center">
+            <div className="grid grid-cols-4 gap-2 p-10 justify-items-center text-kino-grey">
                 {movies ? (
                     movies.map((movie) => {
                         return <MoviePoster movie={movie} key={movie.id} />;
