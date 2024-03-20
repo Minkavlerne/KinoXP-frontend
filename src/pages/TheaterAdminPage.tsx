d8db69cb075c527dc1abcf13f033192363a28d5f
sebbex1337 <sefort@hotmail.dk>
2024-03-20 13:32:24 +0100

    MIN-44 POST theater works in frontend now

--- src/pages/TheaterAdminPage.tsx
+++ src/pages/TheaterAdminPage.tsx
@@ -20,7 +20,7 @@ export default function TheaterAdminPage() {
     return (
         <div className="bg-kino-blue min-h-screen text-kino-white px-10 text-center">
             <h1 className="pb-4">Theater Admin Page</h1>
-            <Link to="/theaters/add" className="w-1/8 px-2 py-2 border-solid rounded bg-kino-grey mx-2">
+            <Link to="/theaters-add" className="w-1/8 px-2 py-2 border-solid rounded bg-kino-grey mx-2">
                 Add Theater
             </Link>
             <div className="pt-10 grid grid-cols-4 gap-2 justify-items-center">
