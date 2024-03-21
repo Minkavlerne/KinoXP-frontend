e23b415d69fe75465a4d40c24a3b46c6d574b654
Jasa95 <jaco3020@stud.kea.dk>
2024-03-21 08:55:41 +0100

    MIN-43
    FUCK THIS

--- src/App.tsx
+++ src/App.tsx
@@ -12,55 +12,57 @@ import RequireAuth from "./security/RequireAuth";
 import Logout from "./security/Logout";
 import MoviesPage from "./pages/MoviesPage";
 import TheaterAdminView from "./components/TheaterAdminView";
+import MovieShowFormPage from "./pages/MovieShowFormPage";
 
 export default function App() {
-    return (
-        <>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/movies" element={<MoviesPage />} />
-                <Route path="/tickets" element={<p>Tickets</p>} />
-                <Route path="/upcoming" element={<UpComingMovies />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/movies/:id" element={<MovieDetailView />} />
-                <Route path="/upcoming/:id" element={<MovieDetailView />} />
-                <Route
-                    path="/theaters"
-                    element={
-                        <RequireAuth roles={["ADMIN"]}>
-                            <TheaterAdminPage />
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="/theaters/:id"
-                    element={
-                        <RequireAuth roles={["ADMIN"]}>
-                            <TheaterAdminView />
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="/theaters-add"
-                    element={
-                        <RequireAuth roles={["ADMIN"]}>
-                            <h1>theaters add form TODO</h1>
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="/movieform"
-                    element={
-                        <RequireAuth roles={["ADMIN"]}>
-                            <MovieFormPage />
-                        </RequireAuth>
-                    }
-                />
-                <Route path="*" element={<h1>Page not found</h1>} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/tickets" element={<p>Tickets</p>} />
+        <Route path="/upcoming" element={<UpComingMovies />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/movies/:id" element={<MovieDetailView />} />
+        <Route path="/upcoming/:id" element={<MovieDetailView />} />
+        <Route path="/movieshowform" element={<MovieShowFormPage />} />
+        <Route
+          path="/theaters"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <TheaterAdminPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/theaters/:id"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <TheaterAdminView />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/theaters-add"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <h1>theaters add form TODO</h1>
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/movieform"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <MovieFormPage />
+            </RequireAuth>
+          }
+        />
+        <Route path="*" element={<h1>Page not found</h1>} />
+      </Routes>
+    </>
+  );
 }
--- src/services/apiFacade.ts
+++ src/services/apiFacade.ts
@@ -1,4 +1,4 @@
-import { Movie, Theater } from "./entityFacade";
+import { Movie, MovieShow, Theater } from "./entityFacade";
 import { handleHttpErrors, makeOptions } from "./fetchUtils";
 import { API_URL } from "../settings";
 const MOVIES_URL = API_URL + "/movies";
@@ -6,6 +6,7 @@ const CATEGORIES_URL = API_URL + "/categories";
 const MOVIE_SHOWS_URL = API_URL + "/movieShows";
 const THEATERS_URL = API_URL + "/theaters";
 
+//Movie FETCHES
 async function getMovies() {
     return fetch(MOVIES_URL).then(handleHttpErrors);
 }
@@ -16,38 +17,64 @@ async function postMovie(movie: Movie) {
     const options = makeOptions("POST", movie);
     return fetch(MOVIES_URL, options).then(handleHttpErrors);
 }
+async function updateMovie(movie: Movie) {
+    const options = makeOptions("PUT", movie);
+    return fetch(MOVIES_URL + "/" + movie.id, options).then(handleHttpErrors);
+}
+async function deleteMovie(id: number) {
+    const options = makeOptions("DELETE", null);
+    return fetch(MOVIES_URL + "/" + id, options).then(handleHttpErrors);
+}
 
+//Category FETCHES
 async function getCategories() {
     return fetch(CATEGORIES_URL).then(handleHttpErrors);
 }
+
+
+//MovieShow FETCHES
 async function getMovieShows() {
     return fetch(MOVIE_SHOWS_URL).then(handleHttpErrors);
 }
+async function getMovieShowById(id:number) {
+    return fetch(MOVIE_SHOWS_URL + "/" + id).then(handleHttpErrors);
+}
+async function postMovieShow(movieShow: MovieShow) {
+    const options = makeOptions("POST", movieShow);
+    return fetch(MOVIE_SHOWS_URL, options).then(handleHttpErrors);
+}
+async function updateMovieShow(movieShow: MovieShow) {
+    const options = makeOptions("PUT", movieShow);
+    return fetch(MOVIE_SHOWS_URL + "/" + movieShow.id, options).then(handleHttpErrors);
+}
+async function deleteMovieShow(id: number) {
+    const options = makeOptions("DELETE", null);
+    return fetch(MOVIE_SHOWS_URL + "/" + id, options).then(handleHttpErrors);
+}
+
+//Upcoming FETCHES
 async function getUpcoming() {
     return fetch(MOVIES_URL + "/upcoming").then(handleHttpErrors);
 }
 
+//Theater FETCHES
 async function getTheaters() {
     return fetch(THEATERS_URL).then(handleHttpErrors);
 }
-
 async function getTheaterById(id: number) {
     return fetch(THEATERS_URL + "/" + id).then(handleHttpErrors);
 }
-
 async function postTheater(theater: Theater) {
     const options = makeOptions("POST", theater);
     return fetch(THEATERS_URL, options).then(handleHttpErrors);
 }
-
 async function updateTheater(theater: Theater) {
     const options = makeOptions("PUT", theater);
     return fetch(THEATERS_URL + "/" + theater.id, options).then(handleHttpErrors);
 }
-
 async function deleteTheater(id: number) {
     const options = makeOptions("DELETE", null);
     return fetch(THEATERS_URL + "/" + id, options).then(handleHttpErrors);
 }
 
-export { getMovies, getMovieById, postMovie, getCategories, getMovieShows, getUpcoming, getTheaters, getTheaterById, postTheater, updateTheater, deleteTheater };
+export { getMovies, getMovieById, postMovie, updateMovie, deleteMovie, getCategories, getMovieShows, getMovieShowById, postMovieShow, updateMovieShow, deleteMovieShow, getUpcoming, getTheaters, getTheaterById, postTheater, updateTheater, deleteTheater };
